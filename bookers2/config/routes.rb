6747278417deda665d447ab93c9fda4f013aaaa8
EC2 Default User <ec2-user@ip-172-31-28-198.ec2.internal>
2021-02-03 01:37:02 +0000

    follow あと一覧のみ

--- bookers2/config/routes.rb
+++ bookers2/config/routes.rb
@@ -7,6 +7,9 @@ Rails.application.routes.draw do
     resources :book_comments, only: [:create, :destroy]
   end 
   
+  post 'follow/:id' => 'relationships#follow', as: 'follow' 
+  post 'unfollow/:id' => 'relationships#unfollow', as: 'unfollow' 
+  
   root 'homes#top'
   get 'home/about' => 'homes#about'
 end
